3f71ff05b02a1315ac0f78b3f3fb07f04c3976a9
Bruno Nobrega <bruno.nobrega@hotmail.com>
2020-11-09 14:48:00 -0300

    Liberando get na rota da tela de login sem autenticação

--- src/modules/companies/infra/http/routes/stores.routes.ts
+++ src/modules/companies/infra/http/routes/stores.routes.ts
@@ -6,12 +6,12 @@ import StoresController from '../controllers/StoresController';
 const storesRouter = Router();
 const storesController = new StoresController();
 
-storesRouter.use(ensureAuthenticated);
+// storesRouter.use(ensureAuthenticated);
 
 storesRouter.get('/', ensureAuthenticated, storesController.index);
 
 storesRouter.get('/:company_cnpj', storesController.show);
 
-storesRouter.post('/', storesController.create);
+storesRouter.post('/', ensureAuthenticated, storesController.create);
 
 export default storesRouter;
