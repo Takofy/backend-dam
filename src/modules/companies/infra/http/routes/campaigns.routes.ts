b6dd06b14d0dd8e7fb35905b3866ff049da7c175
Bruno Nobrega <bruno.nobrega@hotmail.com>
2020-11-09 16:24:19 -0300

    Trocando retorno de store de array para objeto

--- src/modules/companies/infra/http/routes/campaigns.routes.ts
+++ src/modules/companies/infra/http/routes/campaigns.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import CampaignsController from '../controllers/CampaignsController';
 
 const campaignsRouter = Router();
 const campaignsController = new CampaignsController();
+
+campaignsRouter.use(ensureAuthenticated);
 
 campaignsRouter.get('/', campaignsController.index);
 
