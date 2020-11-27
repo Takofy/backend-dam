014f8f214f8235cedb22f3554567764b7628c3fe
Bruno Nobrega <bruno.nobrega@hotmail.com>
2020-11-27 12:52:34 -0300

    Ajustado retorno para upload de vários ativos

--- src/modules/companies/infra/http/controllers/FilesController.ts
+++ src/modules/companies/infra/http/controllers/FilesController.ts
@@ -51,123 +51,70 @@ export default class FilesController {
     // pega arquivos
     const { files } = request;
 
-    // await files.forEach(file => {
-    //   console.log(file.originalname);
-    //   console.log(file.mimetype);
-    // });
-    // return;
+    const allFiles = await Promise.all(
+      files.map(async file => {
+        // upload do ativo
+        const fileUploadService = container.resolve(FileUploadService);
 
-    await files.forEach(file => {
-      // upload do ativo
-      const fileUploadService = container.resolve(FileUploadService);
+        // Ler dimensões do ativo
+        const fileDimensions = sizeOf(file.path);
+        const fileWidh = fileDimensions.width;
+        const fileHeight = fileDimensions.height;
 
-      // Upload file and return hashed file name
-      const fileUploaded = fileUploadService.execute({
-        fileName: file.filename,
-      });
+        // Tamanho do arquivo
+        const size = partial({ locale: 'de' });
+        const fileSize = size(file.size);
+        // const fileSize = 0;
 
-      // Ler dimensões do ativo
-      const fileDimensions = sizeOf(file.path);
-      const fileWidh = fileDimensions.width;
-      const fileHeight = fileDimensions.height;
+        // Salva url com "+" no lugar de " "
+        const s3FileName = file.filename.replace(/\s/g, '+');
+        const fileName = file.filename;
+        const filePath = `${process.env.STORAGE_BASE_PATH}${s3FileName}`;
 
-      // Tamanho do arquivo
-      const size = partial({ locale: 'de' });
-      const fileSize = size(file.size);
-      // const fileSize = 0;
+        const fileMime = file.mimetype.toLowerCase().split('/').shift();
+        const fileType = file.mimetype.toLowerCase().split('/').pop();
 
-      // Salva url com "+" no lugar de " "
-      const s3FileName = file.filename.replace(/\s/g, '+');
-      const fileName = file.filename;
-      const filePath = `${process.env.STORAGE_BASE_PATH}${s3FileName}`;
+        if (request.body.active) {
+          const activeCheck = request.body.active;
+        } else {
+          const activeCheck = true;
+        }
 
-      const fileMime = file.mimetype.toLowerCase().split('/').shift();
-      const fileType = file.mimetype.toLowerCase().split('/').pop();
+        // Upload file and return hashed file name
+        const fileUploaded = await fileUploadService.execute({
+          fileName: file.filename,
+        });
 
-      if (request.body.active) {
-        const activeCheck = request.body.active;
-      } else {
-        const activeCheck = true;
-      }
+        const fileData = {
+          nm_title: file.originalname || 'Sem titulo',
+          nm_description: request.body.nm_description || '',
+          nm_original_file_name: file.originalname || 'non-original-name',
+          nm_type: fileType || 'non-type',
+          nm_subtype: request.body.nm_subtype || 'image',
+          nm_mime: fileMime || 'non-mime',
+          nm_s3_version: request.body.nm_s3_version || '',
+          nm_s3_name: fileName || '',
+          nm_url: filePath || '',
+          nr_code: request.body.nr_code || 0,
+          nr_width: fileWidh || 0,
+          nr_height: fileHeight || 0,
+          nr_size: fileSize || 0,
+          dt_publication: request.body.dt_publication || '2020-01-01',
+          dt_expiration: request.body.dt_expiration || '2020-01-01',
+          campaign_owner_id: request.body.campaign_owner_id,
+          user_owner_id: userId,
+          store_owner_id: storeId,
+          active: activeCheck || true,
+        };
 
-      const fileData = {
-        nm_title: file.originalname || 'Sem titulo',
-        nm_description: request.body.nm_description || '',
-        nm_original_file_name: file.originalname || 'non-original-name',
-        nm_type: fileType || 'non-type',
-        nm_subtype: request.body.nm_subtype || 'image',
-        nm_mime: fileMime || 'non-mime',
-        nm_s3_version: request.body.nm_s3_version || '',
-        nm_s3_name: fileName || '',
-        nm_url: filePath || '',
-        nr_code: request.body.nr_code || 0,
-        nr_width: fileWidh || 0,
-        nr_height: fileHeight || 0,
-        nr_size: fileSize || 0,
-        dt_publication: request.body.dt_publication || '2020-01-01',
-        dt_expiration: request.body.dt_expiration || '2020-01-01',
-        campaign_owner_id: request.body.campaign_owner_id,
-        user_owner_id: userId,
-        store_owner_id: storeId,
-        active: activeCheck || true,
-      };
+        const createFile = container.resolve(CreateFileService);
 
-      const createFile = container.resolve(CreateFileService);
+        const fileFinal = createFile.execute(fileData);
 
-      const file = createFile.execute(fileData);
-    });
+        return await fileFinal;
+      }),
+    );
 
-    // // upload do ativo
-    // const fileUploadService = container.resolve(FileUploadService);
-
-    // // Upload file and return hashed file name
-    // const fileUploaded = await fileUploadService.execute({
-    //   fileName: request.file.filename,
-    // });
-
-    // // Ler dimensões do ativo
-    // const fileDimensions = sizeOf(request.file.path);
-    // const fileWidh = fileDimensions.width;
-    // const fileHeight = fileDimensions.height;
-
-    // // Tamanho do arquivo
-    // const size = partial({ locale: 'de' });
-    // const fileSize = size(request.file.size);
-
-    // // const fileName = request.file.filename.replace(/\s/g, '-');
-    // const fileName = request.file.filename;
-    // const filePath = `${process.env.STORAGE_BASE_PATH}${fileName}`;
-
-    // const fileMime = request.file.mimetype.toLowerCase().split('/').shift();
-    // const fileType = request.file.mimetype.toLowerCase().split('/').pop();
-
-    // const fileData = {
-    //   nm_title: request.body.nm_title || 'Sem titulo',
-    //   nm_description: request.body.nm_description || '',
-    //   nm_original_file_name: request.file.originalname || 'non-original-name',
-    //   nm_type: fileType || 'non-type',
-    //   nm_subtype: request.body.nm_subtype || 'image',
-    //   nm_mime: fileMime || 'non-mime',
-    //   nm_s3_version: request.body.nm_s3_version || '',
-    //   nm_s3_name: fileName || '',
-    //   nm_url: filePath || '',
-    //   nr_code: request.body.nr_code || 0,
-    //   nr_width: fileWidh || 0,
-    //   nr_height: fileHeight || 0,
-    //   nr_size: fileSize || 0,
-    //   dt_publication: request.body.dt_publication || '',
-    //   dt_expiration: request.body.dt_expiration || '',
-    //   campaign_owner_id: request.body.campaign_owner_id,
-    //   user_owner_id: userId,
-    //   store_owner_id: storeId,
-    //   active: request.body.active,
-    // };
-
-    // const createFile = container.resolve(CreateFileService);
-
-    // const file = await createFile.execute(fileData);
-
-    // return response.json(files);
-    return response.send('Upload ok.');
+    return response.status(200).json(allFiles);
   }
 }
